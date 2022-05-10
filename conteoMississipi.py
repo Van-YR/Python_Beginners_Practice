0edff51c41484d0ea2bd116fac60b654bfb0c9ed
vanypr <101904760+vanypr@users.noreply.github.com>
2022-05-10 15:00:06 -0500

    Create conteoMississipi.py

---
+++ conteoMississipi.py
@@ -0,0 +1,10 @@
+ #Código para realizar conteo hasta 5 Mississippi, tarda 1 segundo real en aparecer en la pantalla
+import time
+# Escribe un ciclo for que cuente hasta cinco.
+for i in range(1,6):
+# Cuerpo del ciclo: imprime el número de iteración del ciclo y la palabra "Mississippi".
+    print(i, "Mississipi")
+# Cuerpo del ciclo - uso: time.sleep (1)
+    time.sleep(1)
+# Escribe una función de impresión con el mensaje final.
+print("¡listo o no, ahí voy!")
